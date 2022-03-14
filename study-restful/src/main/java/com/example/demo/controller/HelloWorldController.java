feae98033b7b430b2f8cb5a34cc453f8922888d3
NiiyaDaiki <56012694+NiiyaDaiki@users.noreply.github.com>
2022-03-14 23:48:45 +0900

    pathvariableの利用

--- study-restful/src/main/java/com/example/demo/controller/HelloWorldController.java
+++ study-restful/src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,11 @@ public class HelloWorldController {
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("hello world!!");
 	}
+
+	@GetMapping("/hello-world-bean/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("hello world, %s", name));
+	}
+
 
 }
