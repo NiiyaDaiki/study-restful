ec522851a2af8b948c8b57d4ebb0d52617c1fc4c
NiiyaDaiki <56012694+NiiyaDaiki@users.noreply.github.com>
2022-04-02 21:22:09 +0900

    文言変更

--- study-restful/src/main/java/com/example/demo/controller/HelloWorldController.java
+++ study-restful/src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -13,7 +13,7 @@ public class HelloWorldController {
 
 	@GetMapping("/hello-world")
 	public String helloWorld() {
-		return "hello world!!!!!!!!!!!!";
+		return "hello world!!!!!!!!!!!!!";
 	}
 
 	@GetMapping("/hello-world-bean")
